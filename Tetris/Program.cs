5e145573b9aa3db0ff05f0435820251f585bc0d3
João Chieffi <chieffixgo15@gmail.com>
2021-03-07 01:33:32 -0300

    Criar "campo" de jogo

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -4,9 +4,34 @@ namespace Tetris
 {
     class Program
     {
+        static private int _score = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write("#");
+                Console.Write("\t\t\t");
+                Console.WriteLine("#");
+                if (i == 0)
+                {
+                    Console.Write("#");
+                    Console.Write("\t\t\t");
+                    Console.Write("#");
+                    Console.Write("\t");
+                    Console.WriteLine("SCORE: " + _score);
+                }
+            }
+
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write("#");
+            }
+
+            Console.ReadKey();
         }
     }
 }
